487c1de2a1df7435be4b183f93010f41b2e2d35e
Shunmugathangam <38919461+Shunmugathangam@users.noreply.github.com>
2019-07-24 19:38:12 +0530

    Add files via upload

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,9 +1,15 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugins.firebase.cloudfirestore.CloudFirestorePlugin;
-import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+import com.afur.flutterhtmltopdf.FlutterHtmlToPdfPlugin;
+import com.octapush.moneyformatter.fluttermoneyformatter.FlutterMoneyFormatterPlugin;
+import de.lrosenberg.flutterpdfrenderer.FlutterPdfRendererPlugin;
+import com.eyedeadevelopers.fluttertts.FlutterTtsPlugin;
 import io.flutter.plugins.pathprovider.PathProviderPlugin;
+import io.flutter.plugins.share.SharePlugin;
+import com.zt.shareextend.ShareExtendPlugin;
+import io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin;
+import com.juanito21.simpleshare.SimpleSharePlugin;
 import com.tekartik.sqflite.SqflitePlugin;
 
 /**
@@ -14,9 +20,15 @@ public final class GeneratedPluginRegistrant {
     if (alreadyRegisteredWith(registry)) {
       return;
     }
-    CloudFirestorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.cloudfirestore.CloudFirestorePlugin"));
-    FirebaseCorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.core.FirebaseCorePlugin"));
+    FlutterHtmlToPdfPlugin.registerWith(registry.registrarFor("com.afur.flutterhtmltopdf.FlutterHtmlToPdfPlugin"));
+    FlutterMoneyFormatterPlugin.registerWith(registry.registrarFor("com.octapush.moneyformatter.fluttermoneyformatter.FlutterMoneyFormatterPlugin"));
+    FlutterPdfRendererPlugin.registerWith(registry.registrarFor("de.lrosenberg.flutterpdfrenderer.FlutterPdfRendererPlugin"));
+    FlutterTtsPlugin.registerWith(registry.registrarFor("com.eyedeadevelopers.fluttertts.FlutterTtsPlugin"));
     PathProviderPlugin.registerWith(registry.registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
+    SharePlugin.registerWith(registry.registrarFor("io.flutter.plugins.share.SharePlugin"));
+    ShareExtendPlugin.registerWith(registry.registrarFor("com.zt.shareextend.ShareExtendPlugin"));
+    SharedPreferencesPlugin.registerWith(registry.registrarFor("io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin"));
+    SimpleSharePlugin.registerWith(registry.registrarFor("com.juanito21.simpleshare.SimpleSharePlugin"));
     SqflitePlugin.registerWith(registry.registrarFor("com.tekartik.sqflite.SqflitePlugin"));
   }
 
